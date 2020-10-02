14f141b12a75291327ec03681149728b744ee90b
Mark <supermark11@gmail.com>
2020-10-02 22:17:41 +0300

    failure trying to capture cam via MediaStream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'labaMeets-app';
+
+
+export class AppComponent implements OnInit {
+  preview: any = document.getElementById("preview");
+  recording: any = document.getElementById("recording");
+  startButton: any = document.getElementById("startButton");
+  stopButton: any = document.getElementById("stopButton");
+  downloadButton: any = document.getElementById("downloadButton");
+  logElement: any = document.getElementById("log");
+  stream1: any;
+
+  recordingTimeMS = 5000;
+
+  ngOnInit(): void {
+
+  }
+
+  log(msg: string): void {
+    this.logElement.innerHTML += msg + '\n';
+  }
+
+  wait(delayInMs): Promise<any> {
+    return new Promise(resolve => setTimeout(resolve, delayInMs));
+  }
+
+  startRecording(stream, lengthInMS): Promise<any> {
+    const recorder = new MediaRecorder(stream);
+    const data = [];
+
+    recorder.ondataavailable = event => data.push(event.data);
+    recorder.start();
+    this.log(recorder.state + ' for ' + (lengthInMS/1000) + ' seconds...');
+
+    const stopped = new Promise((resolve, reject) => {
+      recorder.onstop = resolve;
+      recorder.onerror = event => reject(event.timeStamp);
+    });
+
+    const recorded = this.wait(lengthInMS).then(
+      () => recorder.state === 'recording' && recorder.stop()
+    );
+ 
+    return Promise.all([
+      stopped,
+      recorded
+    ])
+    .then(() => data);
+  }
+
+  stop(stream) {
+    stream.getTracks().forEach(track => track.stop());
+  }
+
+  previewing(): void {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(stream => {
+      this.preview.srcObject = stream;
+      this.downloadButton.href = stream;
+      this.stream1 = stream;
+      this.preview.captureStream = this.preview.captureStream || this.preview.mozCaptureStream;
+      return new Promise(resolve => this.preview.onplaying = resolve);
+    }).then(() => this.startRecording(this.preview.captureStream(), this.recordingTimeMS))
+    .then (recordedChunks => {
+      const recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+      this.recording.src = URL.createObjectURL(recordedBlob);
+      this.downloadButton.href = this.recording.src;
+      this.downloadButton.download = "RecordedVideo.webm";
+
+      this.log("Successfully recorded " + recordedBlob.size + " bytes of " +
+          recordedBlob.type + " media.");
+    })
+    .catch(this.log);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
 })
 export class AppModule { }
