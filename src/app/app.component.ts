1b671e9af761f8646e2a590207bd427a09e69320
Mark <supermark11@gmail.com>
2020-10-03 23:59:53 +0300

    camera works via getUserMedia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,70 +11,28 @@ import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 
 export class AppComponent implements OnInit {
-  // toggle webcam on/off
-  public showWebcam = true;
-  public allowCameraSwitch = true;
-  public multipleWebcamsAvailable = false;
-  public deviceId: string;
-  public webCams: MediaDeviceInfo[];
-  public videoOptions: MediaTrackConstraints = {
-    // width: {ideal: 1024},
-    // height: {ideal: 576}
-  };
-  public errors: WebcamInitError[] = [];
+  public video: HTMLVideoElement;
 
-  // latest snapshot
-  public webcamImage: WebcamImage = null;
-
-  // webcam snapshot trigger
-  private trigger: Subject<void> = new Subject<void>();
-  // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
-  private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
-
-  public ngOnInit(): void {
-    WebcamUtil.getAvailableVideoInputs()
-      .then((mediaDevices: MediaDeviceInfo[]) => {
-        this.webCams = mediaDevices;
-        //this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
-      });
-
-      console.log(this.webCams);
+  ngOnInit(): void {
+    this.video = document.getElementById("videoElement") as HTMLVideoElement;
   }
 
-  public triggerSnapshot(): void {
-    this.trigger.next();
+  onButtonClick() {
+    // <reference types="webrtc" />
+    // const getUserMedia = navigator.mediaDevices.getUserMedia ||
+    //                  navigator.getUserMedia ||
+    //                  navigator.webkitGetUserMedia ||
+    //                  navigator.mozGetUserMedia;
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          this.video.srcObject = stream;
+        })
+        .catch((error) => {
+          console.log("Something went wrong!: " + error);
+        });
+    }
   }
 
-  public toggleWebcam(): void {
-    this.showWebcam = !this.showWebcam;
-  }
-
-  public handleInitError(error: WebcamInitError): void {
-    this.errors.push(error);
-  }
-
-  public showNextWebcam(directionOrDeviceId: boolean|string): void {
-    // true => move forward through devices
-    // false => move backwards through devices
-    // string => move to device with given deviceId
-    this.nextWebcam.next(directionOrDeviceId);
-  }
-
-  public handleImage(webcamImage: WebcamImage): void {
-    console.info('received webcam image', webcamImage);
-    this.webcamImage = webcamImage;
-  }
-
-  public cameraWasSwitched(deviceId: string): void {
-    console.log('active device: ' + deviceId);
-    this.deviceId = deviceId;
-  }
-
-  public get triggerObservable(): Observable<void> {
-    return this.trigger.asObservable();
-  }
-
-  public get nextWebcamObservable(): Observable<boolean|string> {
-    return this.nextWebcam.asObservable();
-  }
 }
